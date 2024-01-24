249bf5a6e4bfdea3be1bbf24e3fd95f62311e6ce
Caio Ferreira <147638926+CaioFerreira007@users.noreply.github.com>
2024-01-23 21:32:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+alert("Hello Wolrd!");
+
+// Crie um script que declare duas
+// variáveis e exiba o resultado da soma entre elas.
+
+// const firstNumber = 1;
+// const secondNumber = 3;
+// const sum = firstNumber + secondNumber;
+// alert(sum);
+
+//Crie um script que declare uma variável e
+//verifique se o seu valor é um número. Se for, exiba
+//a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// const numberTrueOrFalse = 1;
+// if (numberOrstring = typeof(number)) {
+//   alert("Esse dado é um número!");
+  
+// } 
+// else {
+//   alert("Esse dado não é um número!");
+// }
+
+//Crie um script que declare uma variável e verifique se o
+//seu valor é uma string. Se for, exiba a mensagem "É uma string", 
+//caso contrário, exiba a mensagem "Não é uma string".
+
+// let numberOrStrig = 
+// function verification (numberOrStrig){
+
+//     numberOrStrig = "23";
+// }
+// if(numberOrStrig != typeof(String)){
+
+//     alert("Esse dado não é uma String!")
+
+// }
+// if( numberOrStrig = typeof(String)){
+//     alert(`Esse dado é uma String!`)
+
+// }
+
+
+//Crie um script que 
+//declare uma variável e verifique se o seu valor é um booleano.
+ //Se for, exiba a mensagem "É um booleano", caso contrário, exiba
+ // a mensagem "Não é um booleano".
+
+// let booleano = true
+// if(booleano = typeof(Boolean)){
+//     alert(`Esse dado é do tipo Boolean`)
+// }
+// else{
+//     alert(`Esse dado não é do tipo Boolean`)
+// }   
+
+
+// Crie um script que declare duas
+//  variáveis e exiba o resultado da subtração entre elas.
+
+
+//Crie um script que declare duas variáveis 
+//e exiba o resultado da subtração entre elas.
+
+
+// const n1 = 54
+// const n2 = 45
+
+// alert(n1 - n2)
+
+
+//Crie um script que declare duas variáveis e
+// exiba o resultado da multiplicação entre elas.
+
+// const numberOne = 4
+// const numberTwo = 78
+// alert(numberOne - numberTwo)
+
+//Crie um script que declare duas variáveis e 
+//exiba o resultado da divisão entre elas.
+
+// const n1 = 7
+// const n2 = 7
+// alert(n1 / n2)
+
+// Crie um script que declare uma variável e verifique
+//  se o seu valor é um número par. Se for, exiba a mensagem
+//   "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// const number = 95
+// if(number % 2 === 0){
+//     alert(`É um número par`)
+// }
+// else{
+//     alert(`Não é um número par`)
+// }
+
+
+//Crie um script que declare uma variável e verifique se
+// o seu valor é um número ímpar. Se for, exiba a mensagem 
+ //"É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+//  const parOuImpar = 4
+//  if (parOuImpar %2 === 1){
+//     alert(`É um número ímpar`)
+//  }
+// else{
+//     alert(`Não é um número ímpar`)
+// }
